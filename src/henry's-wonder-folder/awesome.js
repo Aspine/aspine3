6c12e01fc204b00820cf56c5dcefdcbed0acdb43
cowlyfe <26hsoeiro@cpsd.us>
2025-01-27 18:06:19 -0500

    fixed more aspine

---
+++ src/henry's-wonder-folder/awesome.js
@@ -0,0 +1,60 @@
+function convertToFourScale(gradeTypeArray) {
+	const gradeScale = [
+		{ min: 97, max: 100, gpa: 4.0 },
+		{ min: 93, max: 96, gpa: 4.0 },
+		{ min: 90, max: 92, gpa: 3.7 },
+		{ min: 87, max: 89, gpa: 3.3 },
+		{ min: 83, max: 86, gpa: 3.0 },
+		{ min: 80, max: 82, gpa: 2.7 },
+		{ min: 77, max: 79, gpa: 2.3 },
+		{ min: 73, max: 76, gpa: 2.0 },
+		{ min: 70, max: 72, gpa: 1.7 },
+		{ min: 67, max: 69, gpa: 1.3 },
+		{ min: 65, max: 66, gpa: 1.0 },
+		{ min: 0, max: 64, gpa: 0.0 }
+	];
+	const convertedGrades = [];
+
+	for (let i = 0; i < gradeTypeArray.length; i++) {
+		const percentGrade = gradeTypeArray[i][0];
+		const classType = gradeTypeArray[i][1];
+
+		let gpa = 0.0;
+		for (let j = 0; j < gradeScale.length; j++) {
+			const scale = gradeScale[j];
+			if (percentGrade >= scale.min && percentGrade <= scale.max) {
+				gpa = scale.gpa;
+				break;
+			}
+		}
+		convertedGrades.push([gpa, classType]);
+	}
+	return convertedGrades;
+}
+
+function calcAvgGPA(gradeTypeArray, isWeighted) {
+	let avgGPA = 0;
+
+	for (i in gradeTypeArray) {
+		if (isWeighted == true) {
+			switch (gradeTypeArray[i][1].toLowerCase()) {
+				case 'ap':
+					avgGPA += gradeTypeArray[i][0] + 1.0;
+					break;
+				case 'hn':
+					avgGPA += gradeTypeArray[i][0] + 0.5;
+					break;
+				case 'rg':
+					avgGPA += gradeTypeArray[i][0];
+					break;
+				default:
+					throw new Error(`Invalid typing (${gradeTypeArray[i][1]})`);
+			}
+		} else {
+			avgGPA += gradeTypeArray[i][0];
+		}
+	}
+	return avgGPA / gradeTypeArray.length;
+}
+
+//it just works......
